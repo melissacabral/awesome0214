ab99539609c294b0ae0206f41807a15d1911b253
melissacabral <melissacabral@gmail.com>
2014-02-18 10:52:29 -0800

    adding custom post type templates

--- single-product.php
+++ single-product.php
@@ -8,33 +8,41 @@
 			the_post(); ?>
 
 		<article id="post-<?php the_ID(); ?>" <?php post_class(); ?> >
-			
-			<?php the_post_thumbnail( 'thumbnail', array( 'class' => 'thumb' ) ); ?>
-
 			<h2 class="entry-title"> 
 				<a href="<?php the_permalink(); ?>"> 
 					<?php the_title(); ?> 
 				</a>
 			</h2>
 
+			<?php the_post_thumbnail( 'large', 
+					array( 'class' => 'product-image alignright' ) ); ?>
+
 			<div class="entry-content">
-				<?php the_excerpt(); ?>
+				<?php the_meta(); ?>
 
-				<?php //the_meta(); //list of all custom fields ?>
+				<?php the_terms( $post->ID, 'brand', '<p>Brand: ', '<br />', '</p>' ); ?>
 
+				<?php the_terms( $post->ID, 'feature', '<p>Features: ', ', ', '</p>' ); ?>
+				
 				<?php 
-				//show just the price
-				$price = get_post_meta( $post->ID, 'price', true ); 
-
-				if( $price ):
-					echo '<span class="product-price">$';
-					echo $price;
-					echo '</span>';
-				endif;?>
-
+				//if viewing a single post or page, show full content, otherwise show the excerpt (shortened content)
+				if( is_single() OR is_page() ):
+					the_content();
+					//display pagination if the post needs it (multi-page post)
+					wp_link_pages( array(
+						'before' => '<div class="pagination">',
+						'after' => '</div>',
+						'next_or_number' => 'next',
+						'nextpagelink' => 'Continue reading this post &rarr;',
+						'previouspagelink' => '&larr; Back',
+					) );
+				else:
+					the_excerpt();
+				endif; ?>
 			</div>
-	
+				
 		</article><!-- end post -->
+
 
 		<?php endwhile; ?>
 
